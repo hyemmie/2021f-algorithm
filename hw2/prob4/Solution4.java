672fa83c5a8d0ff3c6946beac481a4f3f8ab0280
hyemmie <lhm0710@naver.com>
2021-10-29 21:37:16 +0900

    feat solution4

--- hw2/prob4/Solution4.java
+++ hw2/prob4/Solution4.java
@@ -4,18 +4,18 @@ import java.io.BufferedReader;
 import java.io.PrintWriter;
 
 /*
-   1. Compile the file with the following command. The class file named Solution4 would be created when you compile the source code.
-       javac Solution4.java -encoding UTF8
+  1. Compile the file with the following command. The class file named Solution4 would be created when you compile the source code.
+      javac Solution4.java -encoding UTF8
 
-   2. Run the compiled program with the following command. Output file(output.txt) should be created after the program is finished
-       java Solution4
+  2. Run the compiled program with the following command. Output file(output.txt) should be created after the program is finished
+      java Solution4
 
-   - The encoding of the source code should be UTF8
-   - You can use the 'time' command to measure your algorithm.
-       time java Solution4
-   - You can also halt the program with the 'timeout' command.
-       timeout 0.5 java Solution4
-       timeout 1 java Solution4
+  - The encoding of the source code should be UTF8
+  - You can use the 'time' command to measure your algorithm.
+    time java Solution4
+  - You can also halt the program with the 'timeout' command.
+      timeout 0.5 java Solution4
+      timeout 1 java Solution4
  */
 
 class Solution4 {
@@ -27,8 +27,8 @@ class Solution4 {
 
 	public static void main(String[] args) throws Exception {
 		/*
-		   Read the input from input.txt
-		   Write your answer to output.txt
+			Read the input from input.txt
+			Write your answer to output.txt
 		 */
 		BufferedReader br = new BufferedReader(new FileReader("input.txt"));
 		StringTokenizer stk;
@@ -36,9 +36,9 @@ class Solution4 {
 
 		for (int test_case = 1; test_case <= 10; test_case++) {
 			/*
-			   n is the number of blocks, and H is the max tower height
-			   read each height of the block to h[0], h[1], ... , h[n-1]
-			   read the heights of the holes to d[0], d[1], ... , d[n-2]
+				n is the number of blocks, and H is the max tower height
+				read each height of the block to h[0], h[1], ... , h[n-1]
+				read the heights of the holes to d[0], d[1], ... , d[n-2]
 			 */
 			stk = new StringTokenizer(br.readLine());
 			n = Integer.parseInt(stk.nextToken()); H = Integer.parseInt(stk.nextToken());
@@ -54,11 +54,12 @@ class Solution4 {
 
 			/////////////////////////////////////////////////////////////////////////////////////////////
 			/*
-			   YOUR CODE HERE
-			   Save your answer to the variable Answer
+				YOUR CODE HERE
+				Save your answer to the variable Answer
 			 */
 			/////////////////////////////////////////////////////////////////////////////////////////////
-			Answer = 0;
+			// Answer = 0;
+			build();
 
 
 			// Print the answer to output.txt
@@ -69,6 +70,42 @@ class Solution4 {
 
 		br.close();
 		pw.close();
+	}
+
+	public static void build() {
+		Answer = 0;
+		int [] temp_block = new int[H+1];
+		int [] confirmed_block = new int[H+1];
+
+		temp_block[h[0]]++;
+
+		for(int block = 1; block < n ; block++){
+			for(int curr = H; curr > 0; curr--){
+				temp_block[h[block]]++;
+				int continous = curr + h[block];
+				int discrete = curr + h[block] - d[block-1];
+				if(discrete <= H) {
+					if(continous <= H){
+						temp_block[continous] += confirmed_block[curr];
+						temp_block[continous] %= 1000000;
+					}
+					temp_block[discrete] += temp_block[curr];
+					temp_block[discrete] %= 1000000;
+					confirmed_block[curr] += temp_block[curr];
+					confirmed_block[curr] %= 1000000;
+					temp_block[curr] = 0;
+				} else {
+					confirmed_block[curr] += temp_block[curr];
+					confirmed_block[curr] %= 1000000;
+					temp_block[curr] = 0;
+				}
+			}
+		}
+
+		for(int sub = 1; sub <= H; sub++){
+			Answer += (temp_block[sub] + confirmed_block[sub]) % 1000000;
+			Answer %= 1000000;
+		}
 	}
 }
 
